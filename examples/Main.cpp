98af514cf0f954db40df978f057207abf69c5b70
klirii <83013244+klirii@users.noreply.github.com>
2024-04-10 16:05:05 +0800

    Update Main.cpp

--- examples/Main.cpp
+++ examples/Main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 	// Initialization of the class object for further work with it, the path or buffer and size must be passed to the constructor
-	ClassFile cf("C:\\Users\\ksafa\\OneDrive\\Рабочий стол\\lunar\\lunar_dump\\net\\minecraft\\client\\renderer\\EntityRenderer.class");
+	ClassFile cf("net\\minecraft\\client\\renderer\\EntityRenderer.class");
 
 	// Search indexes of the name and descriptor of the method whose bytecode you want to change
 	u2 method_name_index = cf.cp_info.find_entry(CONSTANT_Utf8, "getMouseOver");
@@ -25,6 +25,6 @@ int main() {
 	get_mouse_over_code.change_code(513, 1, Bytecodes::_getstatic, fieldref_index);
 	
 	// Class dump to file
-	cf.dump("EntityRenderer.class");
+	cf.dump("path/to/dump");
 	return 0;
  }
